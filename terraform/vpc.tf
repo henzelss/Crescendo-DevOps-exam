db52e0276bade55e7bae1a05dfa6b224e44fabb6
henzelprosperna <henzel@prosperna.com>
2025-02-20 03:15:54 +0800

    Updated AZ

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -12,24 +12,24 @@ resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "ap-southeast-1"
+  availability_zone       = "ap-southeast-1a"
 }
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "ap-southeast-1"
+  availability_zone       = "ap-southeast-1b"
 }
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "ap-southeast-1"
+  availability_zone = "ap-southeast-1a"
 }
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "ap-southeast-1"
+  availability_zone = "ap-southeast-1b"
 }
