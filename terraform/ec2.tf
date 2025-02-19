d14847f490392eb003a7885710c89cfca91ff4ed
henzelprosperna <henzel@prosperna.com>
2025-02-20 03:28:34 +0800

    Removed key pair

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "ami-001c8ac97d79df579"  # Use a valid AWS AMI ID
+  ami           = "ami-001c8ac97d79df579"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet_1.id
-  key_name      = "your-key-pair"
+  # key_name      = "your-key-pair"  # Removed key pair
 
   user_data = <<-EOF
     #!/bin/bash
