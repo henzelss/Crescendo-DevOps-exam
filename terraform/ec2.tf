3642a47f04431b4975201e071b4b0c32f7736c6c
henzelprosperna <henzel@prosperna.com>
2025-02-20 03:21:47 +0800

    Test

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Use a valid AWS AMI ID
+  ami           = "ami-001c8ac97d79df579"  # Use a valid AWS AMI ID
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet_1.id
   key_name      = "your-key-pair"
